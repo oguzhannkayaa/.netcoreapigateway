44bd63d47c0db5b9e36090b1c59381de65fbc959
Oğuzhan <kayaoguzhan@windowslive.com>
2023-03-07 11:00:56 +0300

    reservation API created

---
+++ Services/ResAPI/Reservation.API/Controllers/ReservationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Reservation.API.Infrastructure;
+using Reservation.API.Models;
+
+namespace Reservation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        private IReservationService _reservationService;
+
+        public ReservationController(IReservationService reservationService)
+        {
+            _reservationService = reservationService;
+        }
+
+        [HttpGet("{Id}")]
+        public ReservationDTO Get(int Id)
+        {
+            return _reservationService.GetResByBkgNumber(Id);
+        }
+
+    }
+}
