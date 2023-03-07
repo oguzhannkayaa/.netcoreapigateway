df08976b3d2b8ce08f69e573a412dd9b50b0159c
Oğuzhan <kayaoguzhan@windowslive.com>
2023-03-07 10:27:18 +0300

    project structure created

---
+++ Services/Contact.API/Contact.API.Model/ContactDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact.API.Model
+{
+    public class ContactDTO
+    {
+        public int Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+
+        public String FullName => $"{FirstName}  {LastName}";
+    }
+}
